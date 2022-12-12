67de7ac371c55f35d9df9ef1a772ff637b21e10f
Noah Lee <noahlee0514@gmail.com>
2022-12-12 18:05:41 -0500

    Got replies working

--- SocketServer.c
+++ SocketServer.c
@@ -1,56 +1,60 @@
- #include <stdio.h>
-  #include <sys/types.h> 
-  #include <sys/socket.h>
-  #include <netinet/in.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
-  void error(char *msg)
-  {
-      perror(msg);
-      exit(1);
-  }
+void error(char *msg)
+{
+    perror(msg);
+    exit(1);
+}
 
-   int main(int argc, char *argv[])
-   {
-        int sockfd, newsockfd, portno, clilen;
-        char buffer[256], username[256], message[256];
-        struct sockaddr_in serv_addr, cli_addr;
-        int n;
-        if (argc < 2) {
-            fprintf(stderr,"ERROR, no port provided\n");
-            exit(1);
-        }
-        sockfd = socket(AF_INET, SOCK_STREAM, 0);
-        if (sockfd < 0) 
-           error("ERROR opening socket");
-        bzero((char *) &serv_addr, sizeof(serv_addr));
-        portno = atoi(argv[1]);
-        serv_addr.sin_family = AF_INET;
-        serv_addr.sin_port = htons(portno);
-        serv_addr.sin_addr.s_addr = INADDR_ANY;
-        printf("Enter username: ");
-        bzero(username, 256);        // clear the buffer
-        fgets(username, 255, stdin); // get username from user
-        printf("Waiting for a connection...\n");
-        int length = strlen(username);
-        if (bind(sockfd, (struct sockaddr *) &serv_addr,
-                 sizeof(serv_addr)) < 0) 
-                 error("ERROR on binding"); 
-        listen(sockfd,5); 
-        clilen = sizeof(cli_addr);
-        newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-        if (newsockfd < 0) 
-             error("ERROR on accept");
-        printf("Connection established!\n");
-        bzero(buffer,256);
-        n = read(newsockfd,buffer,255);
-        if (n < 0) error("ERROR reading from socket");
-        printf("%s\n",buffer);
+int main(int argc, char *argv[])
+{
+    int sockfd, newsockfd, portno, clilen;
+    char buffer[256], username[256], message[256];
+    struct sockaddr_in serv_addr, cli_addr;
+    int n;
+    if (argc < 2)
+    {
+        fprintf(stderr, "ERROR, no port provided\n");
+        exit(1);
+    }
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+    portno = atoi(argv[1]);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(portno);
+    serv_addr.sin_addr.s_addr = INADDR_ANY;
+    printf("Enter username: ");
+    bzero(username, 256);        // clear the buffer
+    fgets(username, 255, stdin); // get username from user
+    printf("Waiting for a connection...\n");
+    int length = strlen(username);
+    if (bind(sockfd, (struct sockaddr *)&serv_addr,
+             sizeof(serv_addr)) < 0)
+        error("ERROR on binding");
+    listen(sockfd, 5);
+    clilen = sizeof(cli_addr);
+    newsockfd = accept(sockfd, (struct sockaddr *)&cli_addr, &clilen);
+    if (newsockfd < 0)
+        error("ERROR on accept");
+    printf("Connection established!\n");
+    while (1)
+    {
+        bzero(buffer, 256);
+        n = read(newsockfd, buffer, 255);
+        if (n < 0)
+            error("ERROR reading from socket");
+        printf("%s\n", buffer);
         printf("Please enter the message: ");
         bzero(buffer, 256);
         fgets(buffer, 255, stdin);
         if (buffer[0] == 'e' && buffer[1] == 'x' && buffer[2] == 'i' && buffer[3] == 't')
         {
-            return 0;
+            break;
         }
         // make message buffer a combination of username and message in the format "username: message" in the same line
         strcpy(message, username);
@@ -63,5 +67,6 @@
         if (n < 0)
             error("ERROR writing to socket");
         bzero(buffer, 256);
-        return 0; 
-   }
+    }
+    return 0;
+}
